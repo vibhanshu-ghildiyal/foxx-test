f817785867dbff387bf617667e96a3cf6e2f1c4a
vibhanshu-ghildiyal <vibhanshu@cldcvr.com>
2021-04-18 23:07:12 +0530

    init

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const createRouter = require('@arangodb/foxx/router');
 const router = createRouter();
 
-router.get('/hello-world', function (req, res) {
-    res.send('Hello World!');
-  })
-
 module.context.use(router);
+router.get('/hello-world', function (req, res) {
+res.send({'res':'Hello World!'});
+});
